604fa690a9945c99fa7859c16575b1f6855cd4eb
lumentae <66220444+lumentae@users.noreply.github.com>
2025-10-02 15:47:43 +0200

    Add hash for lattice

--- klassenserver8/mods/lattice.pm
+++ klassenserver8/mods/lattice.pm
@@ -1,8 +1,8 @@
 {
     "download": {
-        "sha1": "",
-        "sha512": "",
-        "size": 0,
+        "sha1": "8e45248ed675b9d486c97478adcaa1983d8f10c7",
+        "sha512": "5e118b7a8d1d80548f9d54773d4d5cb1e81ca46960d301040058be3dc8f6bbaa971b83ae6b532c24ed2da88ad5adb0dd69fd817c934553d3385e2700f48c3355",
+        "size": 48288,
         "url": "https://lumentae.dev/lattice-neoforge-1.21.1-1.1.0.jar"
     },
     "env": {
