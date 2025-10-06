53a1d5a7c4ffe109d64d346ee9c059c0ef6ff9c2
lumentae <66220444+lumentae@users.noreply.github.com>
2025-10-06 20:25:47 +0200

    Add ftb-essentials

---
+++ klassenserver8/mods/ftb-essentials.pm
@@ -0,0 +1,21 @@
+{
+    "download": {
+        "sha1": "40a5fca96e405db57c3f3dd2b3b0baf587e31af8",
+        "sha512": "8fda6da86a3692e7f67ed387a8e462571d41be8f15fbe893a5ad6c0f90b05533b2076002ef06a0f161d5f2ccd380700d1158d91d6acd82ae809a9fe805a0be72",
+        "size": 207898,
+        "url": "https://mediafilez.forgecdn.net/files/6979/622/ftb-essentials-neoforge-2101.1.7.jar"
+    },
+    "env": {
+        "client": "optional",
+        "server": "required"
+    },
+    "id": "ftb-essentials",
+    "name": "ftb-essentials",
+    "platform": "direct",
+    "requirements": [],
+    "slug": "ftb-essentials",
+    "update": {
+        "id": "ftb-essentials",
+        "version": ""
+    }
+}
---
+++ klassenserver8/mods/ftb-library.pm
@@ -0,0 +1,21 @@
+{
+    "download": {
+        "sha1": "cab114cc84c8172f5866df3033174eae1ab71227",
+        "sha512": "cc14a41f15d5438836efb138f6ce247243db7b18ff4b5b7930c11d5aaaacebd7202ff57a1ae85e7ea239645adc2d044b083c4ffee9853e76ab16d54b2083a341",
+        "size": 1387243,
+        "url": "https://mediafilez.forgecdn.net/files/7029/3/ftb-library-neoforge-2101.1.21.jar"
+    },
+    "env": {
+        "client": "optional",
+        "server": "required"
+    },
+    "id": "ftb-library",
+    "name": "ftb-library",
+    "platform": "direct",
+    "requirements": [],
+    "slug": "ftb-library",
+    "update": {
+        "id": "ftb-library",
+        "version": ""
+    }
+}
