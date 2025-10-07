402c7c03d16ec5a1638f66cf242ec3fddc257288
lumentae <66220444+lumentae@users.noreply.github.com>
2025-10-07 21:21:28 +0200

    Add create bluemap

---
+++ klassenserver8/mods/create-bluemap.pm
@@ -0,0 +1,32 @@
+{
+    "download": {
+        "sha1": "c4382b6b4d4c8b6f28580df4dfbd459bf332abb7",
+        "sha512": "af0fa126b234a9a72e0b60f2a661e5c33dacc2a99ccfbf2afd1b0f0e3f82c65a20ddd6844003142e0f949375bcac71ba3051533494c40e5121fd19052b53e6fb",
+        "size": 15535,
+        "url": "https://cdn.modrinth.com/data/W9e7rf1y/versions/ZX7MzWPc/create_bluemap-1.1.0.jar"
+    },
+    "env": {
+        "client": "unsupported",
+        "server": "required"
+    },
+    "id": "create-bluemap",
+    "name": "Create BlueMap",
+    "platform": "modrinth",
+    "requirements": [
+        {
+            "dependency_type": "required",
+            "project_id": "swbUV1cr",
+            "version_id": null
+        },
+        {
+            "dependency_type": "required",
+            "project_id": "LNytGWDc",
+            "version_id": null
+        }
+    ],
+    "slug": "create-bluemap",
+    "update": {
+        "id": "create-bluemap",
+        "version": "ZX7MzWPc"
+    }
+}
