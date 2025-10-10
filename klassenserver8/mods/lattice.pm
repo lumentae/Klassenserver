021e626dcab0f97c1cda931fec1896fe70fe285a
lumentae <66220444+lumentae@users.noreply.github.com>
2025-10-10 15:18:46 +0200

    Update lattice

--- klassenserver8/mods/lattice.pm
+++ klassenserver8/mods/lattice.pm
@@ -1,9 +1,9 @@
 {
     "download": {
-        "sha1": "45fc500e0f4cbb3edea2eb7983b5c41cb7fbe714",
-        "sha512": "b8918b67df3b2e0e31cdbdee4a2386727680754b1fd158ae9120b05ccf3e80c4d93dae5b2500cae20376f5f2777b9dfd7e9bc24f4ca9729794d936426c22c0e2",
-        "size": 53048,
-        "url": "https://cdn.modrinth.com/data/UQBDUlXi/versions/iLO6bJ52/lattice-neoforge-1.21.1-1.2.1.jar"
+        "sha1": "56156b45b9ddb94135c85c7b8067a012d006cc97",
+        "sha512": "f16b3c52aa3eb9cdfedb96e6b31afa074f066a57af637f1cfe019b4f221f2aca6be41b16a0b2255db7c12bbfa071f6e81d28f562b6b3422de4856e9cccd88451",
+        "size": 53117,
+        "url": "https://cdn.modrinth.com/data/UQBDUlXi/versions/kdTD23cj/lattice-neoforge-1.21.1-1.2.2.jar"
     },
     "env": {
         "client": "required",
@@ -16,6 +16,6 @@
     "slug": "lattice",
     "update": {
         "id": "lattice",
-        "version": "iLO6bJ52"
+        "version": "kdTD23cj"
     }
 }
